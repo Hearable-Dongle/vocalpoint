4c592c709342c4cee3efbcd53a2db0d70bd935af
Ryan Jing <ryanjing23@gmail.com>
2026-03-17 10:48:52 -0400

    Integrate latest BLE app and ESP32 logic into main UI

--- vocalpoint_ble/main/src/main.c
+++ vocalpoint_ble/main/src/main.c
@@ -14,10 +14,26 @@
 
 #include "ble_manager.h"
 #include "i2c_bridge.h"
+#include "shared_state.h"
+#include "configs.h"
 #include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "nvs_flash.h"
 
 static const char *s_tag = "app_main";
+
+#if I2C_TESTING_MODE
+static void shared_state_testing_task(void *arg)
+{
+    (void)arg;
+
+    while (1) {
+        vp_state_testing_tick();
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+#endif
 
 void app_main(void)
 {
@@ -28,7 +44,12 @@ void app_main(void)
     }
     ESP_ERROR_CHECK(err);
 
+    vp_state_init();
     i2c_bridge_init();
+
+#if I2C_TESTING_MODE
+    xTaskCreate(shared_state_testing_task, "state_test", 3072, NULL, 1, NULL);
+#endif
 
     err = ble_manager_init();
     if (err != ESP_OK) {
