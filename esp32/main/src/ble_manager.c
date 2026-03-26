2d119d140ef45ff990fef9228b53c5b7d11210d8
Tyler Bell <tyler.bell@rogers.com>
2026-03-26 01:11:28 -0400

    Removing dead Bluetooth code

--- esp32/main/src/ble_manager.c
+++ esp32/main/src/ble_manager.c
@@ -13,7 +13,6 @@
 /**************************************************************************************************/
 
 #include <assert.h>
-#include <inttypes.h>
 #include <stdio.h>
 #include <string.h>
 #include "ble_manager.h"
@@ -32,20 +31,7 @@
 
 static const char *s_tag = "NimBLE_BLE_PRPH";
 
-#if CONFIG_EXAMPLE_RANDOM_ADDR
-static uint8_t s_own_addr_type = BLE_OWN_ADDR_RANDOM;
-#else
 static uint8_t s_own_addr_type;
-#endif
-
-#if CONFIG_EXAMPLE_EXTENDED_ADV
-static uint8_t s_ext_adv_pattern[] = {
-    0x02, 0x01, 0x06,
-    0x03, 0x03, 0xab, 0xcd,
-    0x03, 0x03, 0x18, 0x11,
-    0x11, 0X09, 'n', 'i', 'm', 'b', 'l', 'e', '-', 'b', 'l', 'e', 'p', 'r', 'p', 'h', '-', 'e',
-};
-#endif
 
 /**************************************************************************************************/
 /**
@@ -117,43 +103,6 @@ static void ble_print_conn_desc(struct ble_gap_conn_desc *desc)
                 desc->sec_state.bonded);
 }
 
-#if CONFIG_EXAMPLE_EXTENDED_ADV
-static void ble_start_advertising(void)
-{
-    struct ble_gap_ext_adv_params params;
-    struct os_mbuf *data;
-    uint8_t instance = 0;
-    int rc;
-
-    if (ble_gap_ext_adv_active(instance)) {
-        return;
-    }
-
-    memset(&params, 0, sizeof(params));
-    params.connectable = 1;
-    params.own_addr_type = BLE_OWN_ADDR_PUBLIC;
-    params.primary_phy = BLE_HCI_LE_PHY_1M;
-    params.secondary_phy = BLE_HCI_LE_PHY_2M;
-    params.sid = 1;
-    params.itvl_min = BLE_GAP_ADV_FAST_INTERVAL1_MIN;
-    params.itvl_max = BLE_GAP_ADV_FAST_INTERVAL1_MIN;
-
-    rc = ble_gap_ext_adv_configure(instance, &params, NULL, ble_gap_event_handler, NULL);
-    assert(rc == 0);
-
-    data = os_msys_get_pkthdr(sizeof(s_ext_adv_pattern), 0);
-    assert(data != NULL);
-
-    rc = os_mbuf_append(data, s_ext_adv_pattern, sizeof(s_ext_adv_pattern));
-    assert(rc == 0);
-
-    rc = ble_gap_ext_adv_set_data(instance, data);
-    assert(rc == 0);
-
-    rc = ble_gap_ext_adv_start(instance, 0, 0);
-    assert(rc == 0);
-}
-#else
 static void ble_start_advertising(void)
 {
     struct ble_gap_adv_params adv_params;
@@ -197,20 +146,6 @@ static void ble_start_advertising(void)
         return;
     }
 }
-#endif
-
-#if MYNEWT_VAL(BLE_POWER_CONTROL)
-static void ble_enable_power_control(uint16_t conn_handle)
-{
-    int rc;
-
-    rc = ble_gap_read_remote_transmit_power_level(conn_handle, 0x01);
-    assert(rc == 0);
-
-    rc = ble_gap_set_transmit_power_reporting_enable(conn_handle, 0x1, 0x1);
-    assert(rc == 0);
-}
-#endif
 
 static int ble_gap_event_handler(struct ble_gap_event *event, void *arg)
 {
@@ -241,10 +176,6 @@ static int ble_gap_event_handler(struct ble_gap_event *event, void *arg)
             if (event->connect.status != 0) {
                 ble_start_advertising();
             }
-
-#if MYNEWT_VAL(BLE_POWER_CONTROL)
-            ble_enable_power_control(event->connect.conn_handle);
-#endif
             return 0;
 
         case BLE_GAP_EVENT_DISCONNECT:
@@ -309,97 +240,6 @@ static int ble_gap_event_handler(struct ble_gap_event *event, void *arg)
             assert(rc == 0);
             ble_store_util_delete_peer(&desc.peer_id_addr);
             return BLE_GAP_REPEAT_PAIRING_RETRY;
-
-        case BLE_GAP_EVENT_PASSKEY_ACTION: {
-            ESP_LOGI(s_tag, "PASSKEY_ACTION_EVENT started");
-
-            struct ble_sm_io passkey_io = {0};
-            int key = 0;
-
-            if (event->passkey.params.action == BLE_SM_IOACT_DISP) {
-                passkey_io.action = event->passkey.params.action;
-                passkey_io.passkey = 123456;
-                ESP_LOGI(s_tag,
-                         "Enter passkey %" PRIu32 " on the peer side",
-                         passkey_io.passkey);
-                rc = ble_sm_inject_io(event->passkey.conn_handle, &passkey_io);
-                ESP_LOGI(s_tag, "ble_sm_inject_io result: %d", rc);
-            } else if (event->passkey.params.action == BLE_SM_IOACT_NUMCMP) {
-                ESP_LOGI(s_tag,
-                         "Passkey on device's display: %" PRIu32,
-                         event->passkey.params.numcmp);
-                ESP_LOGI(s_tag,
-                         "Accept or reject passkey via console: key Y or key N");
-
-                passkey_io.action = event->passkey.params.action;
-                if (scli_receive_key(&key)) {
-                    passkey_io.numcmp_accept = key;
-                } else {
-                    passkey_io.numcmp_accept = 0;
-                    ESP_LOGE(s_tag, "Timeout, rejecting key");
-                }
-
-                rc = ble_sm_inject_io(event->passkey.conn_handle, &passkey_io);
-                ESP_LOGI(s_tag, "ble_sm_inject_io result: %d", rc);
-            } else if (event->passkey.params.action == BLE_SM_IOACT_OOB) {
-                static uint8_t oob[16] = {0};
-
-                passkey_io.action = event->passkey.params.action;
-                for (int i = 0; i < 16; i++) {
-                    passkey_io.oob[i] = oob[i];
-                }
-
-                rc = ble_sm_inject_io(event->passkey.conn_handle, &passkey_io);
-                ESP_LOGI(s_tag, "ble_sm_inject_io result: %d", rc);
-            } else if (event->passkey.params.action == BLE_SM_IOACT_INPUT) {
-                ESP_LOGI(s_tag,
-                         "Enter passkey via console in format: key 123456");
-                passkey_io.action = event->passkey.params.action;
-
-                if (scli_receive_key(&key)) {
-                    passkey_io.passkey = key;
-                } else {
-                    passkey_io.passkey = 0;
-                    ESP_LOGE(s_tag, "Timeout, passing 0 as key");
-                }
-
-                rc = ble_sm_inject_io(event->passkey.conn_handle, &passkey_io);
-                ESP_LOGI(s_tag, "ble_sm_inject_io result: %d", rc);
-            }
-
-            return 0;
-        }
-
-        case BLE_GAP_EVENT_AUTHORIZE:
-            MODLOG_DFLT(INFO,
-                        "authorize event: conn_handle=%d attr_handle=%d is_read=%d",
-                        event->authorize.conn_handle,
-                        event->authorize.attr_handle,
-                        event->authorize.is_read);
-            event->authorize.out_response = BLE_GAP_AUTHORIZE_REJECT;
-            return 0;
-
-#if MYNEWT_VAL(BLE_POWER_CONTROL)
-        case BLE_GAP_EVENT_TRANSMIT_POWER:
-            MODLOG_DFLT(INFO,
-                        "Transmit power event : status=%d conn_handle=%d reason=%d phy=%d power_level=%x power_level_flag=%d delta=%d",
-                        event->transmit_power.status,
-                        event->transmit_power.conn_handle,
-                        event->transmit_power.reason,
-                        event->transmit_power.phy,
-                        event->transmit_power.transmit_power_level,
-                        event->transmit_power.transmit_power_level_flag,
-                        event->transmit_power.delta);
-            return 0;
-
-        case BLE_GAP_EVENT_PATHLOSS_THRESHOLD:
-            MODLOG_DFLT(INFO,
-                        "Pathloss threshold event : conn_handle=%d current path loss=%d zone_entered=%d",
-                        event->pathloss_threshold.conn_handle,
-                        event->pathloss_threshold.current_path_loss,
-                        event->pathloss_threshold.zone_entered);
-            return 0;
-#endif
     }
 
     return 0;
@@ -410,30 +250,11 @@ static void ble_on_reset(int reason)
     MODLOG_DFLT(ERROR, "Resetting state; reason=%d\n", reason);
 }
 
-#if CONFIG_EXAMPLE_RANDOM_ADDR
-static void ble_set_random_addr(void)
-{
-    ble_addr_t addr;
-    int rc;
-
-    rc = ble_hs_id_gen_rnd(0, &addr);
-    assert(rc == 0);
-
-    rc = ble_hs_id_set_rnd(addr.val);
-    assert(rc == 0);
-}
-#endif
-
 static void ble_on_sync(void)
 {
     int rc;
 
-#if CONFIG_EXAMPLE_RANDOM_ADDR
-    ble_set_random_addr();
-    rc = ble_hs_util_ensure_addr(1);
-#else
     rc = ble_hs_util_ensure_addr(0);
-#endif
     assert(rc == 0);
 
     rc = ble_hs_id_infer_auto(0, &s_own_addr_type);
@@ -483,23 +304,7 @@ esp_err_t ble_manager_init(void)
     ble_hs_cfg.store_status_cb = ble_store_util_status_rr;
 
     ble_hs_cfg.sm_io_cap = CONFIG_EXAMPLE_IO_TYPE;
-#ifdef CONFIG_EXAMPLE_BONDING
-    ble_hs_cfg.sm_bonding = 1;
-    ble_hs_cfg.sm_our_key_dist |= BLE_SM_PAIR_KEY_DIST_ENC;
-    ble_hs_cfg.sm_their_key_dist |= BLE_SM_PAIR_KEY_DIST_ENC;
-#endif
-#ifdef CONFIG_EXAMPLE_MITM
-    ble_hs_cfg.sm_mitm = 1;
-#endif
-#ifdef CONFIG_EXAMPLE_USE_SC
-    ble_hs_cfg.sm_sc = 1;
-#else
     ble_hs_cfg.sm_sc = 0;
-#endif
-#ifdef CONFIG_EXAMPLE_RESOLVE_PEER_ADDR
-    ble_hs_cfg.sm_our_key_dist |= BLE_SM_PAIR_KEY_DIST_ID;
-    ble_hs_cfg.sm_their_key_dist |= BLE_SM_PAIR_KEY_DIST_ID;
-#endif
 
     rc = ble_gatt_server_init();
     if (rc != 0) {
@@ -515,12 +320,6 @@ esp_err_t ble_manager_init(void)
 
     ble_store_config_init();
     nimble_port_freertos_init(ble_host_task);
-
-    rc = scli_init();
-    if (rc != ESP_OK) {
-        ESP_LOGE(s_tag, "scli_init failed");
-        return ESP_FAIL;
-    }
 
     return ESP_OK;
 }
